1a43d2cc27f2d166426836b2e6895d5e5433a150
Sofiane Hadjadj <sofiane.hadjadj@etu.unilim.fr>
2019-09-28 11:44:47 +0200

    Ajout des Toast dans toutes les fonctions

--- TP1/app/src/main/java/com/hadjimo/tp1/MainActivity.kt
+++ TP1/app/src/main/java/com/hadjimo/tp1/MainActivity.kt
@@ -15,25 +15,37 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
     }
 
     override fun onResume() {
         super.onResume()
+
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
     }
 
     override fun onPause() {
         super.onPause()
+
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
     }
 
     override fun onRestart() {
         super.onRestart()
+
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
     }
 
     override fun onStop() {
         super.onStop()
+
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroy() {
         super.onDestroy()
+
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
     }
 }
