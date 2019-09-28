13551c3c3e77228a6abb1b5f57b3edbe708c913b
Sofiane Hadjadj <sofiane.hadjadj@etu.unilim.fr>
2019-09-28 11:30:54 +0200

    Ajout du premier Toast

--- TP1/app/src/main/java/com/hadjimo/tp1/MainActivity.kt
+++ TP1/app/src/main/java/com/hadjimo/tp1/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.hadjimo.tp1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
     }
 
     override fun onStart() {
