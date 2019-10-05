18f4ec61052740fb97d48d36368bc55d28c39f03
Sofiane Hadjadj <sofiane.hadjadj@etu.unilim.fr>
2019-10-05 22:59:26 +0200

    Fin du TP2

--- TP2/app/src/main/java/com/hadjimo/tp1/ChoixActivity.kt
+++ TP2/app/src/main/java/com/hadjimo/tp1/ChoixActivity.kt
@@ -1,25 +1,36 @@
 package com.hadjimo.tp1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_choix.*
-import org.jetbrains.anko.startActivityForResult
 
 class ChoixActivity : AppCompatActivity() {
+
+    companion object {
+        const val MESSAGE = "ChoixActivity.MESSAGE"
+        const val EXTRA_ISCONFIRMED = "ChoixActivity.ISCONFIRMED"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choix)
+        var texte = intent.getStringExtra(MESSAGE)
+        userName.setText(texte)
+        annuler.setOnClickListener {
+            finish()
 
+        }
         valider.setOnClickListener {
-
-            val message = "test"
-            startActivity<MainActivity>(
-                MainActivity.EXTRA_MESSAGE to message)
-
-
-            startActivity(intent)
+            var choix = "FLURB"
+            if(userName.text.toString() != "")
+            choix = userName.text.toString()
+            val intent = Intent()
+            intent.putExtra(EXTRA_ISCONFIRMED, choix)
+            setResult(RESULT_OK, intent)
+            finish()
         }
 
     }
+
 }
--- TP2/app/src/main/java/com/hadjimo/tp1/MainActivity.kt
+++ TP2/app/src/main/java/com/hadjimo/tp1/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.hadjimo.tp1
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivityForResult
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,29 +13,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
-
-        modifier.setOnClickListener {
-
-            companion object {
-            const val EXTRA_MESSAGE =
-                "ConfirmationActivity.MESSAGE"
-        }
-            override fun onCreate(savedInstanceState: Bundle?) {
-                super.onCreate(savedInstanceState)
-                setContentView(R.layout.choix_activity)
-                val message = intent.getStringExtra(EXTRA_MESSAGE)
-            }
-
-            startActivity(intent)
-        }
-
     }
 
     override fun onStart() {
         super.onStart()
+        var message = textView3.text.toString()
+        modifier.setOnClickListener {
+            startActivityForResult<ChoixActivity>(11, ChoixActivity.MESSAGE to message)
+        }
+    }
 
-        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        try {
+            when (requestCode) {
+                11 -> {
+                    if (resultCode == RESULT_OK) {
+                        val name = data?.getStringExtra(
+                            ChoixActivity.
+                                EXTRA_ISCONFIRMED)
+                            ?: false
+                        textView3.text = name.toString()
+                    }  else if(resultCode == RESULT_CANCELED) {
+                        Toast.makeText(this, "Annulation", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        } finally {
+            Toast.makeText(this, "Fin du processus", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onResume() {
