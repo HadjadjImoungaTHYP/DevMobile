9edf877aa5514b8fb973798a7a3accc4a89ca2b7
IMOUNGA <imounga.r@gmail.com>
2019-10-12 13:01:54 +0200

    Creation de la liste

--- TP3/app/src/main/java/com/hadjimo/tp1/MainActivity.kt
+++ TP3/app/src/main/java/com/hadjimo/tp1/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.hadjimo.tp1
 
-import android.app.Activity
-import android.content.Intent
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivityForResult
 
@@ -13,48 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnModifier.setOnClickListener {
-            startActivityForResult<ChoixActivity>(1, ChoixActivity.EXTRA_ANCIENNE_VALEUR to txtValeur.text)
-        }
+        //recyclerView_main.setBackgroundColor(Color.BLUE)
 
-        // On doit faire un choix tout de suite, mais sans ancienne valeur
-        startActivityForResult<ChoixActivity>(2)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        when(requestCode){
-            1 -> {
-                // Résultat de startActivityForResult<ModifierActivity>
-                if(resultCode == Activity.RESULT_OK){
-                    val valider = data?.getBooleanExtra(ChoixActivity.EXTRA_VALIDER, false) ?: false
-                    if(valider){
-                        // L'utilisateur a utilisé le bouton "valider"
-                        // On récupère la valeur dans l'extra (avec une valeur par défaut de "")
-                        val nouvValeur = data?.getStringExtra(ChoixActivity.EXTRA_VALEUR) ?: ""
-                        // On l'affiche
-                        txtValeur.text = nouvValeur
-                    }else{
-                        // L'utilisateur a utilisé le bouton "annuler"
-                        // on ne fait rien de spécial
-                    }
-                }else if(resultCode == Activity.RESULT_CANCELED){
-                    // L'utilisateur a utilisé le bouton retour <- de son téléphone
-                    // on ne fait rien de spécial non plus
-                }
-            }
-            2 -> {
-                // Résultat de startActivityForResult<ModifierActivity> initial (sans ancienne valeur)
-                if(resultCode == Activity.RESULT_OK){
-                    // On a obligatoirement valider dans ce cas là (pas de bouton annuler)
-                    val nouvValeur = data?.getStringExtra(ChoixActivity.EXTRA_VALEUR) ?: ""
-                    txtValeur.text = nouvValeur
-                }else if(resultCode == Activity.RESULT_CANCELED){
-                    // L'utilisateur a utilisé le bouton retour <- de son téléphone
-                    // on quitte l'appli
-                    finish()
-                }
-            }
-        }
+        recyclerView_main.layoutManager = LinearLayoutManager(this)
+        recyclerView_main.adapter = MainAdapter()
     }
 }
---
+++ TP3/app/src/main/java/com/hadjimo/tp1/MainAdapter.kt
@@ -0,0 +1,32 @@
+package com.hadjimo.tp1
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.video_row.view.*
+
+class MainAdapter: RecyclerView.Adapter<CustomViewHolder>() {
+
+    val videoTItles = listOf("First title", "Second", "3rd")
+
+    // Nombre d'items
+    override fun getItemCount(): Int {
+        return 4
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val layoutInflater = LayoutInflater.from(parent?.context)
+        val cellForRow = layoutInflater.inflate(R.layout.video_row, parent, false)
+        return CustomViewHolder(cellForRow)
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        holder.view?.textView_video_title?.text = "123"
+    }
+
+}
+
+class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+
+}
