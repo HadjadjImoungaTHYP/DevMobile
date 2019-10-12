99c85cbd1fb39a7db38574139526fa39cfcded70
IMOUNGA <imounga.r@gmail.com>
2019-10-12 14:00:44 +0200

    Ajout contenu list

--- TP3/app/src/main/java/com/hadjimo/tp1/MainActivity.kt
+++ TP3/app/src/main/java/com/hadjimo/tp1/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.startActivityForResult
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,5 +16,14 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView_main.layoutManager = LinearLayoutManager(this)
         recyclerView_main.adapter = MainAdapter()
+
+        val homeFeed = HomeFeed(Article(1,"Diplome","Master","Technologie de l'Hypermedia","dmth@youyou.com","Alien1"))
+        val article = Article(1,"Diplome","Master","Technologie de l'Hypermedia","dmth@youyou.com","Alien1")
     }
 }
+
+// titre+auteur+date, nom+prénom+email
+
+class HomeFeed(val article: Article)
+
+class Article(val id: Int, val title: String, val name: String, val prenom: String, val email: String, var auteur: String)
